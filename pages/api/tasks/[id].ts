ddd4b845d890dadb419e75a53183ffdec52995e4
Matt Fantinel <matt@fantinel.dev>
2022-09-04 19:11:37 +0200

    Initial setup + mocked API

---
+++ pages/api/tasks/[id].ts
@@ -0,0 +1,45 @@
+import TaskRepository from '../../../includes/data/tasks';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  let id: number | undefined;
+  if (req.query.id) {
+    id = parseInt(req.query.id as string);
+  }
+
+  if (!id) {
+    res.status(400).json({ name: 'Task ID is required' });
+  } else {
+    if (req.method === 'PUT') {
+      try {
+        const task = req.body;
+        const result = TaskRepository.update(id, task);
+        res.status(200).json(result);
+      } catch (error) {
+        res.status(500).json({ message: handleErrorMessage(error) });
+      }
+    } else if (req.method === 'DELETE') {
+      try {
+        TaskRepository.delete(id);
+        res.status(200).json({ message: 'Task deleted' });
+      } catch (error) {
+        res.status(500).json({ message: handleErrorMessage(error) });
+      }
+    } else if (req.method === 'GET') {
+      try {
+        const task = TaskRepository.getById(id);
+        res.status(200).json(task);
+      } catch (error) {
+        res.status(500).json({ message: handleErrorMessage(error) });
+      }
+    } else {
+      res.status(405).json({ message: 'Method not allowed' });
+    }
+  }
+}
+
+const handleErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message;
+  return String(error);
+};
---
+++ pages/api/tasks/index.ts
@@ -0,0 +1,29 @@
+import TaskRepository from '../../../includes/data/tasks';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      const task = req.body;
+      const result = TaskRepository.create(task);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ message: handleErrorMessage(error) });
+    }
+  } else if (req.method === 'GET') {
+    try {
+      const tasks = TaskRepository.getAll();
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(500).json({ message: handleErrorMessage(error) });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
+
+const handleErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message;
+  return String(error);
+};
